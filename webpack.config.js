db3ed271afa5629e3b8032b3646f6a8fe704ad57
limoza <svacc.nelllely@gmail.com>
2024-01-17 22:21:32 +0900

    implement sass-loader and node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         loader: "url-loader",
         options: {
